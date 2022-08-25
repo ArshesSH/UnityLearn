39aa42ec4cce8bc3f6b23b6653b4a053d99f5170
ArshesSH <105101004ksh@gmail.com>
2022-08-25 10:26:00 +0900

    Added UI for each scene

--- Assets/Resources/Scripts/02 UI/UI_Game.cs
+++ Assets/Resources/Scripts/02 UI/UI_Game.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_Start : MonoBehaviour
+public class UI_Game : MonoBehaviour
 {
-
     private void OnGUI()
     {
-        if ( GUI.Button( new Rect( 400.0f, 250.0f, 150.0f, 30.0f ), "Game Start" ) )
+        if ( GUI.Button( new Rect( 400.0f, 250.0f, 150.0f, 30.0f ), "Check Game Scene" ) )
         {
         }
     }
