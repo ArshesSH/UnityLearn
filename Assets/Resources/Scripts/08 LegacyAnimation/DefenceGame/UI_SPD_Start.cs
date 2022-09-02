8b13e8e991e1ff9ed171c22615099366bbfd83f7
ArshesSH <105101004ksh@gmail.com>
2022-09-02 09:20:23 +0900

    Add 2D Project

--- Assets/Resources/Scripts/08 LegacyAnimation/DefenceGame/UI_SPD_Start.cs
+++ Assets/Resources/Scripts/08 LegacyAnimation/DefenceGame/UI_SPD_Start.cs
@@ -6,10 +6,10 @@ public class UI_SPD_Start : MonoBehaviour
 {
     private void OnGUI()
     {
-        if (GUI.Button(new Rect(1210.0f, 400.0f, 150.0f, 30.0f), "Game Start"))
+        if (GUI.Button(new Rect(810.0f, 400.0f, 150.0f, 30.0f), "Game Start"))
         {
             SPDGameManager.Instance.ChangeScene("08_1 SpartaMain");
         }
-        GUI.Box(new Rect(1000.0f, 400.0f, 180.0f, 220.0f), "\n조작 설명\n\nW,A,S,D: 이동\nAlt: 걷기\nLShift: 뛰기\n마우스 클릭 or LCtrl: 공격\n마우스 휠: 확대\nF: 시점 변경");
+        GUI.Box(new Rect(600.0f, 400.0f, 180.0f, 220.0f), "\n조작 설명\n\nW,A,S,D: 이동\nAlt: 걷기\nLShift: 뛰기\n마우스 클릭 or LCtrl: 공격\n마우스 휠: 확대\nF: 시점 변경");
     }
 }
