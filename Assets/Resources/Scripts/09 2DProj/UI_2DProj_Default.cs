d7e1c07d6867f6d605de896723e19d0f759bcccd
ArshesSH <105101004ksh@gmail.com>
2022-09-02 15:03:44 +0900

    prepare 2dproject

--- Assets/Resources/Scripts/09 2DProj/UI_2DProj_Default.cs
+++ Assets/Resources/Scripts/09 2DProj/UI_2DProj_Default.cs
@@ -6,12 +6,21 @@ using UnityEngine.UI;
 public class UI_2DProj_Default : MonoBehaviour
 {
     public GameObject textObj = null;
+    public Image imgHPBar;
+    int hpMax = 100;
+
     void Start()
     {
         textObj.GetComponent<Text>().text = "”î";
+        ShowHPBar( 50 );
     }
     void Update()
     {
         
+    }
+
+    public void ShowHPBar(int hp)
+    {
+        imgHPBar.fillAmount = (float)hp / (float)hpMax;
     }
 }
