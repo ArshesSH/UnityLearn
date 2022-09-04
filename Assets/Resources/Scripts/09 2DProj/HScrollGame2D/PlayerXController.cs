f49d0ba70dca44109d2b8edb16ed45a67b65ce87
ArshesSH <105101004ksh@gmail.com>
2022-09-04 16:44:14 +0900

    Update Manythings

--- Assets/Resources/Scripts/09 2DProj/HScrollGame2D/PlayerXController.cs
+++ Assets/Resources/Scripts/09 2DProj/HScrollGame2D/PlayerXController.cs
@@ -8,16 +8,21 @@ public class PlayerXController : MonoBehaviour
     {
         Intro,
         Idle,
+        Walk,
         Airbone,
-   
+        Dash,
     }
     
     [Header( "Player Settings" )]
     [SerializeField]
     private float maxHP = 15.0f;
-    private float curHP;
+    public float curHP;
     [SerializeField]
     private float moveSpeed = 200.0f;
+    [SerializeField]
+    private float dashSpeed = 400.0f;
+    [SerializeField]
+    private float dashMaxTime = 3.0f;
     [SerializeField]
     private float jumpSpeed = 100.0f;
     [SerializeField]
@@ -33,62 +38,141 @@ public class PlayerXController : MonoBehaviour
     [SerializeField]
     private string groundLayerName;
 
+    Animator animator;
     Rigidbody2D rb;
     SpriteRenderer spriteRenderer;
-    RaycastHit2D groundHit;
+    RaycastHit2D[] groundHits;
+
+    float dashTimer = 0.0f;
 
     public PlayerState curState;
+    bool isOnGround = false;
+    bool isDashKeyDown = false;
 
-    void Start()
+    private void Awake()
     {
-        curState = PlayerState.Intro;
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    void Start()
+    {
+        curHP = maxHP;
+        curState = PlayerState.Intro;
+    }
+
     void Update()
     {
-        curState = CheckGround() ? PlayerState.Idle : PlayerState.Airbone;
+        
+        if (!IsCurState("X_Intro"))
+        {
+            isOnGround = CheckGround();
+            if(!isOnGround )
+            {
+                curState = PlayerState.Airbone;
+            }
+
+            PlayerInput();
+            animator.SetBool("IsOnGround", isOnGround);
+            animator.SetBool("IsDashNow", curState == PlayerState.Dash);
+        }
+
     }
 
     private void FixedUpdate()
     {
-        MovePlayerX();
-        JumpPlayerX();
         ApplyGravity();
-        print( rb.velocity.y );
+        if (!IsCurState("X_Intro"))
+        {
+            MovePlayerX();
+        }
     }
 
-    void ApplyGravity()
+    bool IsCurState(string stateName)
     {
-        if(curState == PlayerState.Airbone)
+        return animator.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+    }
+
+    void PlayerInput()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
         {
-            float gravitySpeed = Mathf.Min( rb.velocity.y - gravity, maxGravitySpeed );
-            rb.velocity = new Vector2( rb.velocity.x, gravitySpeed );
+            JumpPlayerX();
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetTrigger("Shoot");
+        }
+
+        if(Input.GetButton("Dash"))
+        {
+            isDashKeyDown = true;
+            dashTimer += Time.deltaTime;
+            if( dashTimer <= dashMaxTime)
+            {
+                curState = PlayerState.Dash;
+            }
+            else
+            {
+                curState = PlayerState.Idle;
+            }
+        }
+        if(Input.GetButtonUp("Dash"))
+        {
+            isDashKeyDown = false;
+            curState = PlayerState.Idle;
+            dashTimer = 0.0f;
+        }
+    }
+
+    public void ApplyGravity()
+    {
+        if(!CheckGround())
+        {
+            float gravitySpeed = Mathf.Min(rb.velocity.y - gravity, maxGravitySpeed);
+            rb.velocity = new Vector2(rb.velocity.x, gravitySpeed);
         }
     }
 
     void MovePlayerX()
     {
-        float x = Input.GetAxis( "Horizontal" );
-        if( x < 0.0f)
+        if( curState == PlayerState.Dash )
         {
-            spriteRenderer.flipX = false;
+            rb.velocity = new Vector2( spriteRenderer.flipX ? dashSpeed : -dashSpeed , rb.velocity.y);
         }
-        else if( x > 0.0f)
+        else
         {
-            spriteRenderer.flipX = true;
-        }
+            float x = Input.GetAxis("Horizontal");
+            animator.SetFloat("Horizontal", Mathf.Abs(x));
+            if (x < 0.0f)
+            {
+                spriteRenderer.flipX = false;
+                curState = PlayerState.Walk;
+            }
+            else if (x > 0.0f)
+            {
+                spriteRenderer.flipX = true;
+                curState = PlayerState.Walk;
+            }
+            else
+            {
+                curState = PlayerState.Idle;
+            }
 
-        rb.velocity = new Vector2(moveSpeed * x, rb.velocity.y);
+            rb.velocity = new Vector2(moveSpeed * x, rb.velocity.y);
+        }
     }
+    
+
+
 
     void JumpPlayerX()
     {
-        if(Input.GetKeyDown(KeyCode.X))
+        if(CheckGround())
         {
-            print( "jump" );
-            rb.AddForce( jumpSpeed * Vector2.up, ForceMode2D.Impulse );
+            rb.AddForce(jumpSpeed * Vector2.up, ForceMode2D.Impulse);
         }
     }
 
@@ -96,19 +180,33 @@ public class PlayerXController : MonoBehaviour
     {
         if( groundRayStartPos != null && groundRayEndPos != null)
         {
-            groundHit = Physics2D.Linecast( groundRayStartPos.transform.position, groundRayEndPos.transform.position );
-            if ( groundHit.collider.gameObject.layer == LayerMask.NameToLayer( groundLayerName ) )
+            groundHits = Physics2D.LinecastAll(groundRayStartPos.transform.position, groundRayEndPos.transform.position);
+            foreach( var hit in groundHits )
             {
-                return true;
+                if(hit.collider.gameObject.layer == LayerMask.NameToLayer(groundLayerName))
+                {
+                    return true;
+                }
             }
             return false;
         }
         return false;
     }
 
+    
+
+
     private void OnDrawGizmos()
     {
         
     }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(0.0f, 0.0f, 150.0f, 30.0f), "OnGround: " + isOnGround.ToString());
+        GUI.Box(new Rect(0.0f, 30.0f, 150.0f, 30.0f), "State: " + curState);
+        GUI.Box(new Rect(0.0f, 60.0f, 150.0f, 30.0f), "dashTime: " + dashTimer.ToString());
+    }
+
 
 }
